b1f0156a06a661dedfafe8f88c1d46f85543a2ca
Faizacahya <faizacahyapradani001@gmail.com>
2023-11-16 10:42:54 +0700

    first commit

--- controllers/user.js
+++ controllers/user.js
@@ -25,16 +25,23 @@ const User = require('../models/User')
       }
       
     },
-    store: (req, res) => {
-        users.push(req.body)
-        res.json({
+    store: async (req, res) => {
+      try {
+        const user = await User.create(req.body)
+        res.status(200).json({
           status: true,
-          data: users,
+          data: user,
           method: req.method,
           url: req.url,
           message: "Data Berhasil Ditambahkan"
          }) 
-      },
+        
+      } catch (error) {
+        res.status(400).json({success: false})
+        
+      }
+    },  
+
       update: (req, res) => {
         const id = req.params.id
         users.filter(user => {
